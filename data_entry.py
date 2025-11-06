a1e5461c5d947e3dac3b93c4405d7e90259570e2
Stephen <stephendpurdue@gmail.com>
2025-11-06 10:33:19 +0000

    Setup

--- data_entry.py
+++ data_entry.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 date_format = "%D-%M-%Y"
+CATEGORIES = {"I": "Income", "E": "Expense"}
 
 # Gets date from user.
 def get_date(prompt, allow_default=False):
@@ -16,14 +17,24 @@ def get_date(prompt, allow_default=False):
 
     # This function is recursive, meaning it runs until a valid date is entered.
 
-
 def get_amount():
-    pass
-
+    try:
+        amount = float(input("Enter the amount: "))
+        if amount <= 0:
+            raise ValueError("Amount must be greater than zero.") # Checks if the amount is greater than zero.
+        return amount
+    except ValueError as e:
+        print(e)
+        return get_amount()
 
 def get_category():
-    pass
+    category = input("Enter the category: ('I' for Income or 'E' for Expense): ").upper()
+    if category in CATEGORIES:
+        return CATEGORIES[category]
+
+    print("Invalid category. Please enter 'I' for Income or 'E' for Expense.")
+    return get_category()
 
 
 def get_description():
-    pass
+    return input("Enter a description: (Optional) ")
